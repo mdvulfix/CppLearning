87a808aab7906d4f49326e0ac7819b55fa791b66
mdvulfix <54022658+mdvulfix@users.noreply.github.com>
2022-09-06 17:05:55 +0300

    Start update
    Start update

---
+++ Basics/app.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() 
+{
+    string name = "";
+
+    cout << "Please, write your name!" << endl;
+    cin >> name;
+    cout << "Hello " << name << "!" << endl;
+
+    int numberof = 100;
+
+}
